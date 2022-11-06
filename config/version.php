31950dec513875045a3b524071dfac5752fb6118
Pavan Akshith <pavansanapala99@gmail.com>
2022-11-06 17:34:06 +0530

    first commit

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v6.0.13',
+  'full_app_version' => 'v6.0.13 - build 8923-g30f4af3ac',
+  'build_version' => '8923',
+  'prerelease_version' => '',
+  'hash_version' => 'g30f4af3ac',
+  'full_hash' => 'v6.0.13-97-g30f4af3ac',
+  'branch' => 'master',
+);
